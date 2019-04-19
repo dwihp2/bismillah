59a84a69a7df8a8b8f8420cf2d4843af95e671e6
Dwi Heryanto <m.dwiheryanto2@gmail.com>
2019-04-19 17:49:40 +0700

    add feature for displaying icon in bottom tab

--- src/screen/dashboard/index.js
+++ src/screen/dashboard/index.js
@@ -1,6 +1,33 @@
+import React from 'react';
 import ProfileScreen from './ProfileScreen';
 import HomeScreen from './HomeScreen';
 import {createBottomTabNavigator} from 'react-navigation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const bottomTabNavigatorConfig = {
+    tabBarOptions: {
+        activeTintColor: "tomato",
+        inactiveTintColor: "grey",
+        labelStyle: {fontSize:15}
+    },
+    defNavOpts: ({navigation}) => ({
+        tabBarIcon: ({ focused, tintColor }) => {
+            const {routeName} = navigation.state;
+            let iconName;
+            switch (routeName) {
+                case "HOME":
+                    iconName = "md-home";
+                    break;
+                case "PROFILE":
+                    iconName = "md-person";
+                    break;
+                default:
+                    break;
+            }
+            return <Ionicons name = {iconName} size={25} color={tintColor}/>;
+        }
+    })
+}
 
 export default createBottomTabNavigator ({
     HOME: {
@@ -9,4 +36,7 @@ export default createBottomTabNavigator ({
     PROFILE:{
         screen:ProfileScreen
     }
+},{
+    tabBarOptions:bottomTabNavigatorConfig.tabBarOptions,
+    defaultNavigationOptions:bottomTabNavigatorConfig.defNavOpts
 })
